f1cf31a0279f50623ff4e8d04137897486b88a5b
YBD21 <santosh.deuja2000@gmail.com>
2023-08-06 11:40:00 +0545

    learning Build-In Types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
 
 // console.log("Hello World");
 
+// Demo
 let age: number = 20;
 
 if (age < 50)
     age +=10;
 
  console.log(age);   
+
+
+
+/* 
+Fundamentals 
+
+1. The any Type
+2. Arrays
+3. Tupels
+4. Enums
+5. Functions
+6. Objects
+
+*/
+
+// Build-In Types
+
+/*  
+
+JavaScript               TypeScript
+                         
+1. number                1. any
+2. string                2. unknown
+3. boolean               3. never
+4. null                  4. enum
+5. undefined             5. tuple
+6. object
+
+*/
+
+
+let sales : number = 123_456;
+let course : string = 'TypeScript';
+let level : any;
+let exp; // by default  any
+
