ea31a5b6deecad9f956beaeb42061993970299ec
YBD21 <santosh.deuja2000@gmail.com>
2023-10-12 08:38:10 +0545

    added interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 
+const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1'
+// type of information that exist 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-
-axios.get(url).then(respond => {
-    console.log(respond.data)
-})
+axios.get(url).then((respond) => {
+  const todo = respond.data as Todo; // compare with interface todo
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  console.log(` The Todo With ID : ${id}`);
+});
