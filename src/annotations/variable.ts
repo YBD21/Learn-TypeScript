357b69f24160670cffbe436ad77820156ff2a7c4
YBD21 <santosh.deuja2000@gmail.com>
2023-10-21 14:39:03 +0545

    added function example

--- src/annotations/variable.ts
+++ src/annotations/variable.ts
@@ -41,4 +41,10 @@ let myCar: Car = new Car()
 let point:{x: number; y:number} = {
     x:10,
     y:20
+};
+
+
+// Function
+const logNumber: (i:number) => void = (i:number) => {
+    console.log(i)
 }
