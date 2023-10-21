e210d424cd9cf434573576916ad2fae0f9518939
YBD21 <santosh.deuja2000@gmail.com>
2023-10-21 14:18:48 +0545

    added variable

---
+++ src/annotations/variable.ts
@@ -0,0 +1,23 @@
+// veriable declearation of annotation
+const numberOfapple: number = 5;
+
+// numberOfapple = 'Ten' -- error
+
+/*
+Types :
+  1. Primitive Types : 
+                       number 
+                       boolean
+                       void
+                       undefined
+                       string
+                       symbol
+                       null
+
+  2. Object Types : 
+                       functions
+                       arrays
+                       classes
+                       objects
+                       
+*/
