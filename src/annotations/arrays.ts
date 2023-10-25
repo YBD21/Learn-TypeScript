a4448c364b4a0782055e9075d1888121dc3879d3
YBD21 <santosh.deuja2000@gmail.com>
2023-10-25 07:40:37 +0545

    added arrays.ts in annotations

---
+++ src/annotations/arrays.ts
@@ -0,0 +1,13 @@
+const carMakers = ["ford", "toyota", "chevy"];
+const dates = [new Date(), new Date()];
+
+const carsByMaker: string[][] = [];
+
+// Help with inference when extracting values
+
+const car = carMakers[0];
+const yourCar = carMakers.pop();
+
+// Prevent incompatiable values
+// carMakers.push(100);
+
