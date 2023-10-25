c36060efe5164f12919aa4486f675a33695de1c7
YBD21 <santosh.deuja2000@gmail.com>
2023-10-25 08:12:38 +0545

    added tuple.ts

---
+++ src/annotations/tuple.ts
@@ -0,0 +1,9 @@
+// Tuple - Array-like structure where each element represents some property of a record.
+
+const drink = {
+  color: "brown",
+  carbonated: false,
+  suger: 40,
+};
+
+// Example of Tuple
