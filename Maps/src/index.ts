b8df73bdbd28bd0265a0e6fe9d91651906ebb116
YBD21 <santosh.deuja2000@gmail.com>
2023-10-26 21:24:37 +0545

    added google map to div

--- Maps/src/index.ts
+++ Maps/src/index.ts
@@ -1,11 +1,20 @@
 /// <reference types="@types/google.maps" />
-import { User } from "./components/User";
-import { Company } from "./components/Company";
+// import { User } from "./components/User";
+// import { Company } from "./components/Company";
 
-const user = new User();
+// const user = new User();
 
-console.log(user);
+// console.log(user);
 
-const company = new Company();
+// const company = new Company();
 
-console.log(company);
+// console.log(company);
+
+const id: any = document.getElementById("map");
+new google.maps.Map(id, {
+  zoom: 3,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
