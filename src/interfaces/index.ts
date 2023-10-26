34dbe0e3040cf1a482d887e0f0416e243220b51e
YBD21 <santosh.deuja2000@gmail.com>
2023-10-26 10:17:38 +0545

    update interfaces folder with example

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -25,19 +25,20 @@ interface Vehicle {
   name: string;
   year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
-    name: "civic",
-    year: new Date,
-    broken: true,
-  };
-
+  name: "civic",
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name : ${this.name}`;
+  },
+};
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name : ${vehicle.name}`);
-  console.log(`Year : ${vehicle.year}`);
-  console.log(`Broken ?  ${vehicle.name}`);
+  console.log(vehicle);
 };
 
 printVehicle(oldCivic);
