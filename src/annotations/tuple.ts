9d968172cd925a4cd81e5273c6c0ec2499e4a109
YBD21 <santosh.deuja2000@gmail.com>
2023-10-26 08:44:18 +0545

    added observation tuple vs object

--- src/annotations/tuple.ts
+++ src/annotations/tuple.ts
@@ -12,3 +12,13 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ["brown", true, 40];
 const sprint: Drink = ["clear", true, 40];
 const cokeZero: Drink = ["Borwn", true, 0];
+
+
+// hard to understand what that data is
+const carSpecs : [number, number] = [400,3354];
+
+// clear to understand what data is with key and value.
+const carStats = {
+  horsepower : 440,
+  weight: 3354
+}
