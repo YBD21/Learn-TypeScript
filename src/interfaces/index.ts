20a2084dfed599f7bd4a81404affbffa03ff696f
YBD21 <santosh.deuja2000@gmail.com>
2023-10-26 09:22:58 +0545

    added interfaces folder with example

---
+++ src/interfaces/index.ts
@@ -0,0 +1,43 @@
+// Example of Long annotations
+/*
+// object that define a car
+const oldCivic = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+};
+
+const printVehicle = (vehicle: {
+  name: string;
+  year: number;
+  broken: boolean;
+}): void => {
+  console.log(`Name : ${vehicle.name}`);
+  console.log(`Year : ${vehicle.year}`);
+  console.log(`Broken ?  ${vehicle.name}`);
+};
+*/
+
+// Fix long annotation with interface
+
+// creating a new type
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic = {
+    name: "civic",
+    year: 2000,
+    broken: true,
+  };
+
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name : ${vehicle.name}`);
+  console.log(`Year : ${vehicle.year}`);
+  console.log(`Broken ?  ${vehicle.name}`);
+};
+
+printVehicle(oldCivic);
