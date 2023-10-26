bbdc39a04befd6d53b28a16cbbd7dee6163ac561
YBD21 <santosh.deuja2000@gmail.com>
2023-10-26 20:45:00 +0545

    loaded company in index.ts

--- Maps/src/index.ts
+++ Maps/src/index.ts
@@ -1,5 +1,10 @@
 import { User } from "./components/User";
+import { Company } from "./components/Company";
 
 const user = new User();
 
 console.log(user);
+
+const company = new Company();
+
+console.log(company);
