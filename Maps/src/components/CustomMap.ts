228e98b4d271c071779ea2a6f95e7b9452d39ec2
YBD21 <santosh.deuja2000@gmail.com>
2023-10-28 10:54:57 +0545

    added implements to Classes

--- Maps/src/components/CustomMap.ts
+++ Maps/src/components/CustomMap.ts
@@ -2,13 +2,14 @@
 
 // Instruction to every other class
 // on how they can be an arugment to 'addMarker'
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
 
   markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
